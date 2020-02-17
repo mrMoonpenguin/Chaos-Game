45d8414c9435b86ccbc7f2c8be71baab0fad98dc
Hasbi Dulger <hasbi.dulger@student.kuleuven.be>
2020-02-17 19:08:29 +0100

    Minor adjustments

--- ChaosGame.py
+++ ChaosGame.py
@@ -1,25 +1,21 @@
 import turtle, random
 
-A = (0, 300)
-B = (-300, -200)
-C = (300, -200)
-
-polygon = [A, B, C]
-
 
 def getAvg(P1, P2):
     return (P1[0] + P2[0]) / 2, (P1[1] + P2[1]) / 2
 
 
-def draw():
-    start = (random.randint(-400, 400), random.randint(-400, 400))
-    drawPoint(start)
-    current = start
+def draw(polygon):
+    for vertex in polygon:
+        drawPoint(vertex)
+
+    point = (random.randint(-400, 400), random.randint(-400, 400))
+    drawPoint(point)
 
     for i in range(10**4):
         x = random.randint(0, len(polygon) - 1)
-        current = getAvg(polygon[x], current)
-        drawPoint(current)
+        point = getAvg(polygon[x], point)
+        drawPoint(point)
 
 
 def drawPoint(coordinates):
@@ -32,13 +28,17 @@ def setup():
     turtle.penup()
     turtle.delay(0.01)
 
-    for vertex in polygon:
-        drawPoint(vertex)
-
 
 def main():
     setup()
-    draw()
+
+    A = (0, 300)
+    B = (-300, -200)
+    C = (300, -200)
+
+    polygon = [A, B, C]
+
+    draw(polygon)
 
     turtle.exitonclick()
 
